dc813859c021ed360ab87603c2ba3627c92c4d3a
KIBUNDULU KAZADI INNOCENT <kibundulukazadiinnocent@MacBook-Air-de-KIBUNDULU.local>
2024-11-08 21:25:54 +0100

    Dashboards readjastements

--- School/views.py
+++ School/views.py
@@ -25,9 +25,12 @@ def students_school_dashboard(request):
 def communicate_school_dashboard(request):
     return render(request, 'dashboard/school/communicate.html')
 
+
+
+
 #STUDENT_DASHBOARD
-def home_student_dashboard(request):
-   return render(request, 'dashboard/student/home.html')
+def class_student_dashboard(request):
+   return render(request, 'dashboard/student/class.html')
 
 def student_dashboard(request):
     context = {
@@ -50,17 +53,86 @@ def attendance_student_dashboard(request):
 def character_student_dashboard(request):
    return render(request, 'dashboard/student/character.html')
 
-def seating_student_dashboard(request):
-   return render(request, 'dashboard/student/seating.html')
-
 def communicate_student_dashboard(request):
     return render(request, 'dashboard/student/communicate.html')
 
-def reports_student_dashboard(request):
-    return render(request, 'dashboard/student/reports.html')
+def library_student_dashboard(request):
+    return render(request, 'dashboard/student/library.html')
 
-def teach_student_dashboard(request):
-    return render(request, 'dashboard/student/teach.html')
+def reward_student_dashboard(request):
+    return render(request, 'dashboard/student/reward.html')
 
-def polis_student_dashboard(request):
-    return render(request, 'dashboard/student/polis.html')
+def tutor_student_dashboard(request):
+    return render(request, 'dashboard/student/tutor.html')
+
+
+
+#PARENT_DASHBOARD
+def class_parent_dashboard(request):
+   return render(request, 'dashboard/parent/class.html')
+
+def parent_dashboard(request):
+    context = {
+        'username': request.user.username,
+        'firstname': request.user.first_name,
+        'lastname': request.user.last_name,
+        'email': request.user.email,
+    }
+    return render(request, 'dashboard/parent/parent_dashboard.html', context)
+
+def assignments_parent_dashboard(request):
+   return render(request, 'dashboard/parent/assignments.html')
+
+def gradebook_parent_dashboard(request):
+   return render(request, 'dashboard/parent/gradebook.html')
+
+def attendance_parent_dashboard(request):
+   return render(request, 'dashboard/parent/attendance.html')
+
+def communicate_parent_dashboard(request):
+    return render(request, 'dashboard/parent/communicate.html')
+
+def library_parent_dashboard(request):
+    return render(request, 'dashboard/parent/library.html')
+
+
+
+#TEACHER_DASHBOARD
+def home_teacher_dashboard(request):
+   return render(request, 'dashboard/teacher/home.html')
+
+def teacher_dashboard(request):
+    context = {
+        'username': request.user.username,
+        'firstname': request.user.first_name,
+        'lastname': request.user.last_name,
+        'email': request.user.email,
+    }
+    return render(request, 'dashboard/teacher/teacher_dashboard.html', context)
+
+def assignments_teacher_dashboard(request):
+   return render(request, 'dashboard/teacher/assignments.html')
+
+def attendance_teacher_dashboard(request):
+   return render(request, 'dashboard/teacher/attendance.html')
+
+def character_teacher_dashboard(request):
+   return render(request, 'dashboard/teacher/character.html')
+
+def communicate_teacher_dashboard(request):
+    return render(request, 'dashboard/teacher/communicate.html')
+
+def gradebook_teacher_dashboard(request):
+   return render(request, 'dashboard/teacher/gradebook.html')
+
+def polis_teacher_dashboard(request):
+    return render(request, 'dashboard/teacher/polis.html')
+
+def reports_teacher_dashboard(request):
+    return render(request, 'dashboard/teacher/reports.html')
+
+def seating_teacher_dashboard(request):
+    return render(request, 'dashboard/teacher/seating.html')
+
+def teach_teacher_dashboard(request):
+    return render(request, 'dashboard/teacher/teach.html')
