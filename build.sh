bfbb32426b7c30b5fcf59ee4f5f6f2a37b3a17e6
Jonlandu <jonlandu78@gmail.com>
2025-01-17 01:34:38 +0100

    Use fakes data to cover the issue

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 set -o errexit
 pip install -r requirements.txt
+python manage.py migrate Authentication --fake
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
