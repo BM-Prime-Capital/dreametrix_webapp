8ceb42b4a4468f0eac33fb3486b6a029cdb64513
Jonlandu <jonlandu78@gmail.com>
2025-01-17 12:19:02 +0100

    Deletion of the migration file

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 set -o errexit
 pip install -r requirements.txt
-python manage.py migrate Authentication zero
-python manage.py makemigrations Authentication
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
