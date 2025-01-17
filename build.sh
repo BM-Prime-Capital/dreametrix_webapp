dda5d877b9212084ca3ef1e2bd3974245d019bf7
Jonlandu <jonlandu78@gmail.com>
2025-01-17 01:44:45 +0100

    Deleteed fakes

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 set -o errexit
 pip install -r requirements.txt
-python manage.py migrate Authentication --fake
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
