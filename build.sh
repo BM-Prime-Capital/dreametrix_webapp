72e7bd6327cb6ad3eaa277bea0db13d6a72c982c
Jonlandu <jonlandu78@gmail.com>
2025-01-30 00:07:53 +0100

    Classe, assignmen, and gradebook functionalities

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+python manage.py populate_classes
+python manage.py populate_assignments
+python manage.py populate_gradebooks
