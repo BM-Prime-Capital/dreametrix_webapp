1571c4f378c4ba02617fd3b2439c989e23d75fe1
Jonlandu <jonlandu78@gmail.com>
2025-02-07 21:00:28 +0100

    Faker improvements

--- School/management/commands/populate_gradebooks.py
+++ School/management/commands/populate_gradebooks.py
@@ -23,24 +23,21 @@ class Command(BaseCommand):
             self.stdout.write(self.style.WARNING('Aucun étudiant trouvé dans la base de données.'))
             return
 
-        for _ in range(30):  # Ajustez le nombre de grades à créer
+        for _ in range(30):  # Ajustez le nombre de notes à créer
             class_instance = random.choice(classes)  # Sélectionner une classe aléatoire
             student = random.choice(students)  # Sélectionner un étudiant aléatoire
-            average = round(random.uniform(50, 100), 2)  # Générer une moyenne aléatoire
+            score = round(random.uniform(50, 100), 2)  # Générer un score aléatoire
 
-            # Créer des fichiers de feedback fictifs (remplacez par vos propres fichiers si nécessaire)
-            exam_feedback = f"feedback/exam_feedback_{student.user.username}.txt"
-            test_feedback = f"feedback/test_feedback_{student.user.username}.txt"
-            homework_feedback = f"feedback/homework_feedback_{student.user.username}.txt"
+            # Créer des fichiers de feedback fictifs
+            feedback_file = f"feedback/feedback_{student.user.username}_{random.randint(1, 100)}.txt"
 
             # Créer et enregistrer la nouvelle instance de Gradebook
             new_gradebook_entry = Gradebook.objects.create(
                 class_instance=class_instance,
                 student=student,
-                average=average,
-                exam_feedback=exam_feedback,
-                test_feedback=test_feedback,
-                homework_feedback=homework_feedback
+                assessment_type=random.choice([choice[0] for choice in Gradebook.ASSESSMENT_TYPES]),  # Sélectionner un type d'évaluation aléatoire
+                feedback_file=feedback_file,
+                score=score
             )
 
         self.stdout.write(self.style.SUCCESS('Gradebooks peuplés avec succès avec des données factices !'))
